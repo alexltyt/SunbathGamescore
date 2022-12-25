d9a2970cd0c5839b661d990ac84efbe6f2de52ac
alexltyt <alexltytca@gmail.com>
2022-12-25 10:41:56 -0700

    big 2 add restore function

--- scripts/score.js
+++ scripts/score.js
@@ -27,6 +27,7 @@ const submit = document.getElementById("submit");
 const undo = document.getElementById("undo");
 const clear = document.getElementById("clear");
 const hint_input = document.getElementById("hint_input");
+const restoredata = document.getElementById("restoredata");
 
 
 var p1Score = 0;
@@ -39,6 +40,7 @@ var p3Total = 0;
 var p4Total = 0;
 var scoreList = [];
 var scoreListRecord = [];
+let restore = false;
 
 document.querySelector('#inputSection').style.display = 'none';
 player1box.addEventListener("click",()=>{
@@ -55,10 +57,8 @@ player1box.addEventListener("click",()=>{
     p2ScoreInput.value = p2ScoreInput.defaultValue;
     p3ScoreInput.value = p3ScoreInput.defaultValue;
     p4ScoreInput.value = p4ScoreInput.defaultValue;
-    resetTimer.style.display = 'none';
-    hint_input.style.display = "grid";
-
 })
+
 player2box.addEventListener("click",()=>{
     document.querySelector('#inputSection').style.display = 'flex';
     player2box.classList.replace("players","winners")
@@ -73,8 +73,6 @@ player2box.addEventListener("click",()=>{
     p1ScoreInput.value = p2ScoreInput.defaultValue;
     p3ScoreInput.value = p3ScoreInput.defaultValue;
     p4ScoreInput.value = p4ScoreInput.defaultValue;
-    resetTimer.style.display = 'none';
-    hint_input.style.display = "grid";
 
 })
 player3box.addEventListener("click",()=>{
@@ -91,10 +89,8 @@ player3box.addEventListener("click",()=>{
     p1ScoreInput.value = p2ScoreInput.defaultValue;
     p2ScoreInput.value = p3ScoreInput.defaultValue;
     p4ScoreInput.value = p4ScoreInput.defaultValue;
-    resetTimer.style.display = 'none';
-    hint_input.style.display = "grid";
-
 })
+
 player4box.addEventListener("click",()=>{
     document.querySelector('#inputSection').style.display = 'flex';
     player4box.classList.replace("players","winners")
@@ -109,9 +105,6 @@ player4box.addEventListener("click",()=>{
     p1ScoreInput.value = p2ScoreInput.defaultValue;
     p2ScoreInput.value = p3ScoreInput.defaultValue;
     p3ScoreInput.value = p4ScoreInput.defaultValue;
-    resetTimer.style.display = 'none';
-    hint_input.style.display = "grid";
-
 })
 
 
@@ -124,24 +117,28 @@ editP1Name.addEventListener("click",()=>{
     document.getElementById("p1Name").innerHTML = p1Name;
     document.getElementById("p1Namebtm").innerHTML = p1Name; 
     p1ScoreInput.placeholder = p1Name;
+    localStorage.p1Name = p1Name;
 });
 editP2Name.addEventListener("click",()=>{
     p2Name = prompt("Please enter a new Name.")
     document.getElementById("p2Name").innerHTML = p2Name;
     document.getElementById("p2Namebtm").innerHTML = p2Name; 
     p2ScoreInput.placeholder = p2Name;
+    localStorage.p2Name = p2Name;
 });
 editP3Name.addEventListener("click",()=>{
     p3Name = prompt("Please enter a new Name.")
     document.getElementById("p3Name").innerHTML = p3Name;
     document.getElementById("p3Namebtm").innerHTML = p3Name; 
     p3ScoreInput.placeholder = p3Name;
+    localStorage.p3Name = p3Name;
 });
 editP4Name.addEventListener("click",()=>{
     p4Name = prompt("Please enter a new Name.")
     document.getElementById("p4Name").innerHTML = p4Name;
     document.getElementById("p4Namebtm").innerHTML = p4Name; 
     p4ScoreInput.placeholder = p4Name;
+    localStorage.p4Name = p4Name;
 });
 
 submit.addEventListener("click", roundFinish);
@@ -164,8 +161,48 @@ clear.addEventListener("click", ()=>{
         p4Total = 0;
         calTotal();
         eachRoundContainer.innerHTML = "";
+        localStorage.clear();
+        document.getElementById("p1Name").innerHTML = "Player1";
+        document.getElementById("p1Namebtm").innerHTML = "Player1"; 
+        document.getElementById("p2Name").innerHTML = "Player2";
+        document.getElementById("p2Namebtm").innerHTML = "Player2"; 
+        document.getElementById("p3Name").innerHTML = "Player3";
+        document.getElementById("p3Namebtm").innerHTML = "Player3"; 
+        document.getElementById("p4Name").innerHTML = "Player4";
+        document.getElementById("p4Namebtm").innerHTML = "Player4"; 
     }
 });
+
+restoredata.addEventListener("click",()=>{
+    let text = "Are you sure to restore data?";
+    if (confirm(text) == true) {
+        restore = true;
+        p1Score = localStorage.p1Score;
+        p2Score = localStorage.p2Score;
+        p3Score = localStorage.p3Score;
+        p4Score = localStorage.p4Score;
+        p1Name = localStorage.p1Name;
+        p2Name = localStorage.p2Name;
+        p3Name = localStorage.p3Name;
+        p4Name = localStorage.p4Name;
+        scoreListRecord = JSON.parse(localStorage.scoreListRecord);
+        showScore();
+        document.getElementById("p1Name").innerHTML = p1Name;
+        document.getElementById("p1Namebtm").innerHTML = p1Name;
+        document.getElementById("p2Name").innerHTML = p2Name;
+        document.getElementById("p2Namebtm").innerHTML = p2Name;
+        document.getElementById("p3Name").innerHTML = p3Name;
+        document.getElementById("p3Namebtm").innerHTML = p3Name;
+        document.getElementById("p4Name").innerHTML = p4Name;
+        document.getElementById("p4Namebtm").innerHTML = p4Name;
+        p1ScoreInput.placeholder = p1Name;
+        p2ScoreInput.placeholder = p2Name;
+        p3ScoreInput.placeholder = p3Name;
+        p4ScoreInput.placeholder = p4Name;
+        scoreboardDisplay();
+    }
+})
+
 
 function doubleOrTriple(){
     if (parseInt(p1ScoreInput.value) >=10 && parseInt(p1ScoreInput.value) <=12){
@@ -244,75 +281,66 @@ function showScore(){
     p4ScoreDisplay.innerHTML = 0-p4Score;
 }
 
-function scoreboardDisplay(){
-    var eachRound = document.createElement("div");
-    eachRound.classList.add("eachRound");
-    const p1ScoreDisplay = document.createElement("div");
-    p1ScoreDisplay.classList.add("scorebox")
-    const p2ScoreDisplay = document.createElement("div");
-    p2ScoreDisplay.classList.add("scorebox")
-    const p3ScoreDisplay = document.createElement("div");
-    p3ScoreDisplay.classList.add("scorebox")
-    const p4ScoreDisplay = document.createElement("div");
-    p4ScoreDisplay.classList.add("scorebox")
-    const p1eachround = document.createTextNode(String(scoreListRecord.slice(-1)[0][0]).padStart(2," "));
-    const p2eachround = document.createTextNode(String(scoreListRecord.slice(-1)[0][1]).padStart(2," "));
-    const p3eachround = document.createTextNode(String(scoreListRecord.slice(-1)[0][2]).padStart(2," "));
-    const p4eachround = document.createTextNode(String(scoreListRecord.slice(-1)[0][3]).padStart(2," "));
-    p1ScoreDisplay.appendChild(p1eachround);
-    p2ScoreDisplay.appendChild(p2eachround);
-    p3ScoreDisplay.appendChild(p3eachround);
-    p4ScoreDisplay.appendChild(p4eachround);
-    eachRound.appendChild(p1ScoreDisplay);
-    eachRound.appendChild(p2ScoreDisplay);
-    eachRound.appendChild(p3ScoreDisplay);
-    eachRound.appendChild(p4ScoreDisplay);
-    eachRoundContainer.appendChild(eachRound);
-}
 
+function scoreboardDisplay(){
+    if (restore == true){
+        for (var i = 0; i <scoreListRecord.length; i++){
+            var eachRound = document.createElement("div");
+            eachRound.classList.add("eachRound");
+            const p1ScoreDisplay = document.createElement("div");
+            p1ScoreDisplay.classList.add("scorebox");
+            const p2ScoreDisplay = document.createElement("div");
+            p2ScoreDisplay.classList.add("scorebox");
+            const p3ScoreDisplay = document.createElement("div");
+            p3ScoreDisplay.classList.add("scorebox");
+            const p4ScoreDisplay = document.createElement("div");
+            p4ScoreDisplay.classList.add("scorebox");
+            const p1eachround = document.createTextNode(String(scoreListRecord.slice(i)[0][0]).padStart(2," "));
+            const p2eachround = document.createTextNode(String(scoreListRecord.slice(i)[0][1]).padStart(2," "));
+            const p3eachround = document.createTextNode(String(scoreListRecord.slice(i)[0][2]).padStart(2," "));
+            const p4eachround = document.createTextNode(String(scoreListRecord.slice(i)[0][3]).padStart(2," "));
+            p1ScoreDisplay.appendChild(p1eachround);
+            p2ScoreDisplay.appendChild(p2eachround);
+            p3ScoreDisplay.appendChild(p3eachround);
+            p4ScoreDisplay.appendChild(p4eachround);
+            eachRound.appendChild(p1ScoreDisplay);
+            eachRound.appendChild(p2ScoreDisplay);
+            eachRound.appendChild(p3ScoreDisplay);
+            eachRound.appendChild(p4ScoreDisplay);
+            eachRoundContainer.appendChild(eachRound);
+            restore = false;
+        }
+    }else{
+        var eachRound = document.createElement("div");
+        eachRound.classList.add("eachRound");
+        const p1ScoreDisplay = document.createElement("div");
+        p1ScoreDisplay.classList.add("scorebox");
+        const p2ScoreDisplay = document.createElement("div");
+        p2ScoreDisplay.classList.add("scorebox");
+        const p3ScoreDisplay = document.createElement("div");
+        p3ScoreDisplay.classList.add("scorebox");
+        const p4ScoreDisplay = document.createElement("div");
+        p4ScoreDisplay.classList.add("scorebox");
+        const p1eachround = document.createTextNode(String(scoreListRecord.slice(-1)[0][0]).padStart(2," "));
+        const p2eachround = document.createTextNode(String(scoreListRecord.slice(-1)[0][1]).padStart(2," "));
+        const p3eachround = document.createTextNode(String(scoreListRecord.slice(-1)[0][2]).padStart(2," "));
+        const p4eachround = document.createTextNode(String(scoreListRecord.slice(-1)[0][3]).padStart(2," "));
+        p1ScoreDisplay.appendChild(p1eachround);
+        p2ScoreDisplay.appendChild(p2eachround);
+        p3ScoreDisplay.appendChild(p3eachround);
+        p4ScoreDisplay.appendChild(p4eachround);
+        eachRound.appendChild(p1ScoreDisplay);
+        eachRound.appendChild(p2ScoreDisplay);
+        eachRound.appendChild(p3ScoreDisplay);
+        eachRound.appendChild(p4ScoreDisplay);
+        eachRoundContainer.appendChild(eachRound);
+    }
+}
 
 window.onbeforeunload = function (e) {
     return "";
 };
 
-//Timer//
-let timeSecond = 61;
-const timeH = document.querySelector("h1");
-const resetTimer = document.getElementById("resetTimer");
-const timer = document.getElementById("timer");
-displayTime(timeSecond);
-
-
-resetTimer.style.display = 'none';
-resetTimer.addEventListener("click",()=>{
-    timeSecond = 61;
-});
-timer.addEventListener("click",()=>{
-    resetTimer.style.display = 'flex';
-    timeSecond = 61;
-})
-
-const countDown = setInterval(() => {
-  timeSecond--;
-  displayTime(timeSecond);
-  if (timeSecond == 0 || timeSecond < 1) {
-    endCount();
-    // clearInterval(countDown);
-    timeSecond = 61;
-  }
-}, 1000);
-
-function displayTime(second) {
-  const min = Math.floor(second / 60);
-  const sec = Math.floor(second % 60);
-  timeH.innerHTML = `
-  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
-  `;
-}
-
-function endCount() {
-  timeH.innerHTML = "Time out";
-}
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
@@ -321,7 +349,7 @@ function openNav() {
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-  }
+}
